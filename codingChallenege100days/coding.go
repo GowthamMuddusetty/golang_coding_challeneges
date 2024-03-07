517c3853f8760a35bb3cdb73083f1e941170bf7c
MUDDUSETTY GOWTHAM <chintugowtham83@gmail.com>
2024-03-07 11:16:24 +0530

    Update coding.go
    Question description for 1st one

--- codingChallenege100days/coding.go
+++ codingChallenege100days/coding.go
@@ -5,6 +5,8 @@ import (
 	"math"
 )
 
+/ /1.  Given a signed 32-bit integer x, return x with its digits reversed.
+// If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.")
 func Reverse(x int) int {
 	reversed := 0
 	sign := 1
